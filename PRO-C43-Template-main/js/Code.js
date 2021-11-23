3aedc4551b6374a588ce2fd5ab0d289766949ec0
ashish200000 <78677208+ashish200000@users.noreply.github.com>
2021-11-23 10:51:12 +0530

    Add files via upload

---
+++ PRO-C43-Template-main/js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "MIGRATION";
+const accessCode2 = "ENVIRONMENT";
+const accessCode3 = "TEMPERATURE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text(" M T O I G R N I A", 100,50)
+    fill("lightblue")
+    text("Hint: When Birds Don't Like Winter Season !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("E T R O N I V N M N E", 700,150)
+    fill("lightblue")
+    text("Hint: It Is All Around Us !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("E M E E R T R U A P T", 100,250)
+    fill("lightblue")
+    text("Hint: If You Catch A Cold!!", 100,270)
+
+}
